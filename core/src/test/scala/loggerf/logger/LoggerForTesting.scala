19a19c0d357564bd3bce17cb8153347686124901
Kevin Lee <kevin.code@kevinlee.io>
2020-08-01 21:33:23 +1000

    Close #90 - Add a way to set an internal Logger when creating loggerf.logger.Logger

--- core/src/test/scala/loggerf/logger/LoggerForTesting.scala
+++ core/src/test/scala/loggerf/logger/LoggerForTesting.scala
@@ -1,30 +1,63 @@
 package loggerf.logger
 
+import loggerf.logger.LoggerForTesting.MessageKeeper
+
 /**
  * @author Kevin Lee
  * @since 2020-04-12
  */
-@SuppressWarnings(Array("org.wartremover.warts.Var"))
 final case class LoggerForTesting private (
-  private var debugMessages: Vector[String]
-, private var infoMessages: Vector[String]
-, private var warnMessages: Vector[String]
-, private var errorMessages: Vector[String]
+  logger: MessageKeeper
 ) extends Logger {
   override def debug(message: String): Unit =
-    debugMessages = debugMessages :+ message
+    logger.debugMessages = logger.debugMessages :+ message
 
   override def info(message: String): Unit =
-    infoMessages = infoMessages :+ message
+    logger.infoMessages = logger.infoMessages :+ message
 
   override def warn(message: String): Unit =
-    warnMessages = warnMessages :+ message
+    logger.warnMessages = logger.warnMessages :+ message
 
   override def error(message: String): Unit =
-    errorMessages = errorMessages :+ message
+    logger.errorMessages = logger.errorMessages :+ message
 }
 
 object LoggerForTesting {
+
+  @SuppressWarnings(Array("org.wartremover.warts.Var"))
+  final case class MessageKeeper(
+    private var _debugMessages: Vector[String],
+    private var _infoMessages: Vector[String],
+    private var _warnMessages: Vector[String],
+    private var _errorMessages: Vector[String]
+  ) {
+    def debugMessages: Vector[String] = _debugMessages
+    private[LoggerForTesting] def debugMessages_=(debugMessages: Vector[String]): Unit =
+      _debugMessages = debugMessages
+
+    def infoMessages: Vector[String] = _infoMessages
+    private[LoggerForTesting] def infoMessages_=(infoMessages: Vector[String]): Unit =
+      _infoMessages = infoMessages
+
+    def warnMessages: Vector[String] = _warnMessages
+    private[LoggerForTesting] def warnMessages_=(warnMessages: Vector[String]): Unit =
+      _warnMessages = warnMessages
+
+    def errorMessages: Vector[String] = _errorMessages
+    private[LoggerForTesting] def errorMessages_=(errorMessages: Vector[String]): Unit =
+      _errorMessages = errorMessages
+  }
+
+  def apply(
+    debugMessages: Vector[String],
+    infoMessages: Vector[String],
+    warnMessages: Vector[String],
+    errorMessages: Vector[String]
+  ): LoggerForTesting =
+    LoggerForTesting(MessageKeeper(debugMessages, infoMessages, warnMessages, errorMessages))
+
   def apply(): LoggerForTesting =
-    LoggerForTesting(Vector.empty, Vector.empty, Vector.empty, Vector.empty)
+    LoggerForTesting(
+      MessageKeeper(Vector.empty, Vector.empty, Vector.empty, Vector.empty)
+    )
 }
--- sbt-logging/src/main/scala/loggerf/logger/SbtLogger.scala
+++ sbt-logging/src/main/scala/loggerf/logger/SbtLogger.scala
@@ -1,14 +1,14 @@
 package loggerf.logger
 
-final class SbtLogger(val log: sbt.util.Logger) extends Logger {
+final class SbtLogger(val logger: sbt.util.Logger) extends Logger {
 
-  override def debug(message: String): Unit = log.debug(message)
+  override def debug(message: String): Unit = logger.debug(message)
 
-  override def info(message: String): Unit = log.info(message)
+  override def info(message: String): Unit = logger.info(message)
 
-  override def warn(message: String): Unit = log.warn(message)
+  override def warn(message: String): Unit = logger.warn(message)
 
-  override def error(message: String): Unit = log.error(message)
+  override def error(message: String): Unit = logger.error(message)
 }
 
 object SbtLogger {
