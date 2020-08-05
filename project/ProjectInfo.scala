d4d65323bdceab96748aae7eb1ab0737fcbdc61b
Kevin Lee <kevin.code@kevinlee.io>
2020-08-06 00:54:40 +1000

    Prepare to release v1.2.0

--- project/ProjectInfo.scala
+++ project/ProjectInfo.scala
@@ -4,7 +4,7 @@ object ProjectInfo {
 
   final case class ProjectName(projectName: String) extends AnyVal
 
-  val ProjectVersion: String = "1.1.0"
+  val ProjectVersion: String = "1.2.0"
 
   def commonWarts(scalaBinaryVersion: String): Seq[wartremover.Wart] = scalaBinaryVersion match {
     case "2.10" =>
