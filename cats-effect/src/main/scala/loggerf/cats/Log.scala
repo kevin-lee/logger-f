049a4b0d254ec73cda53984766255f08b5714526
Kevin Lee <kevin.code@kevinlee.io>
2020-10-10 21:02:00 +1100

    Close #120 - Add logPure to Log

--- cats-effect/src/main/scala/loggerf/cats/Log.scala
+++ cats-effect/src/main/scala/loggerf/cats/Log.scala
@@ -31,6 +31,14 @@ trait Log[F[_]] {
       }
     }
 
+  def logPure[A](fa: F[A])(toLeveledMessage: A => LeveledMessage with NotIgnorable): F[A] =
+    MF0.flatMap(fa) { a =>
+      toLeveledMessage(a) match {
+        case LeveledMessage.LogMessage(message, level) =>
+          effectOf(getLogger(canLog, level)(message)) *> pureOf(a)
+      }
+    }
+
   def log[A](
       foa: F[Option[A]]
     )(
@@ -53,6 +61,31 @@ trait Log[F[_]] {
 
           case LeveledMessage.Ignore =>
             effectOf(a.some)
+        }
+    }
+
+  def logPure[A](
+      foa: F[Option[A]]
+    )(
+      ifEmpty: => LeveledMessage with MaybeIgnorable
+    , toLeveledMessage: A => LeveledMessage with MaybeIgnorable
+    ): F[Option[A]] =
+    MF0.flatMap(foa) {
+      case None =>
+        ifEmpty match {
+          case LeveledMessage.Ignore =>
+            pureOf(none[A])
+
+          case LeveledMessage.LogMessage(message, level) =>
+            effectOf(getLogger(canLog, level)(message)) *> pureOf(none[A])
+        }
+      case Some(a) =>
+        toLeveledMessage(a) match {
+          case LeveledMessage.LogMessage(message, level) =>
+            effectOf(getLogger(canLog, level)(message)) *> pureOf(a.some)
+
+          case LeveledMessage.Ignore =>
+            pureOf(a.some)
         }
     }
 
@@ -81,6 +114,31 @@ trait Log[F[_]] {
       }
   }
 
+  def logPure[A, B](
+      feab: F[Either[A, B]]
+    )(
+      leftToMessage: A => LeveledMessage with MaybeIgnorable
+    , rightToMessage: B => LeveledMessage with MaybeIgnorable
+    ): F[Either[A, B]] =
+    MF0.flatMap(feab) {
+    case Left(l) =>
+      leftToMessage(l) match {
+        case LeveledMessage.LogMessage(message, level) =>
+          effectOf(getLogger(canLog, level)(message)) *> pureOf(l.asLeft[B])
+
+        case LeveledMessage.Ignore =>
+          pureOf(l.asLeft[B])
+      }
+    case Right(r) =>
+      rightToMessage(r) match {
+        case LeveledMessage.LogMessage(message, level) =>
+          effectOf(getLogger(canLog, level)(message)) *> pureOf(r.asRight[A])
+
+        case LeveledMessage.Ignore =>
+          pureOf(r.asRight[A])
+      }
+  }
+
   def log[A](
       otfa: OptionT[F, A]
     )(
@@ -88,6 +146,14 @@ trait Log[F[_]] {
     , toLeveledMessage: A => LeveledMessage with MaybeIgnorable
     ): OptionT[F, A] =
     OptionT(log(otfa.value)(ifEmpty, toLeveledMessage))
+
+  def logPure[A](
+      otfa: OptionT[F, A]
+    )(
+      ifEmpty: => LeveledMessage with MaybeIgnorable
+    , toLeveledMessage: A => LeveledMessage with MaybeIgnorable
+    ): OptionT[F, A] =
+    OptionT(logPure(otfa.value)(ifEmpty, toLeveledMessage))
 
 
   def log[A, B](
@@ -97,6 +163,14 @@ trait Log[F[_]] {
     , rightToMessage: B => LeveledMessage with MaybeIgnorable
     ): EitherT[F, A, B] =
     EitherT(log(etfab.value)(leftToMessage, rightToMessage))
+
+  def logPure[A, B](
+      etfab: EitherT[F, A, B]
+    )(
+      leftToMessage: A => LeveledMessage with MaybeIgnorable
+    , rightToMessage: B => LeveledMessage with MaybeIgnorable
+    ): EitherT[F, A, B] =
+    EitherT(logPure(etfab.value)(leftToMessage, rightToMessage))
 
 }
 
