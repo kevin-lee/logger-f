beb722fef388d8a9bcceea00c6dda7e2e373836a
Kevin Lee <kevin.code@kevinlee.io>
2025-08-08 00:40:44 +1000

    Fix `build.sbt` and GitHub Actions config - replace hard-coded `orphan-cats` with `libs.orphanCats` - GitHub Actions: Remove `-XX:+UseJVMCICompiler` from `GH_SBT_OPTS` - GitHub Actions: Replace `-Xmx4G` with `-Xmx8G`

--- .github/workflows/sbt-build-all.sh
+++ .github/workflows/sbt-build-all.sh
@@ -28,9 +28,9 @@ else
 #    echo "report build but it does nothing for now."
   fi
 
-#  echo "sbt -J-Xmx4G ++${scala_version}! -v clean ${test_task}"
+#  echo "sbt -J-Xmx8G ++${scala_version}! -v clean ${test_task}"
 #  sbt \
-#    -J-Xmx4G \
+#    -J-Xmx8G \
 #    ++${scala_version}! \
 #    -v \
 #    clean \
@@ -42,7 +42,6 @@ else
   if [[ "$CURRENT_BRANCH_NAME" == "main" || "$CURRENT_BRANCH_NAME" == "logger-f-1" || "$CURRENT_BRANCH_NAME" == "release" ]]
   then
     sbt \
-      -J-Xmx4G \
       ++${scala_version}! \
       -v \
       clean \
@@ -50,7 +49,6 @@ else
       packagedArtifacts
   else
     sbt \
-      -J-Xmx4G \
       ++${scala_version}! \
       -v \
       clean \
