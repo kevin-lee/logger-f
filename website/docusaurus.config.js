c0082dc407ae7a29e8e336f1fc723af339017fce
Kevin Lee <kevin.code@kevinlee.io>
2025-09-28 16:37:55 +1000

    Update docs: with the latest version info [skip ci]

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -120,7 +120,7 @@ const websiteConfig = {
               "path": "v1",
             },
             "current": {
-              "label": "2.0.0",
+              "label": "2.3.0",
             },
           }
         },
